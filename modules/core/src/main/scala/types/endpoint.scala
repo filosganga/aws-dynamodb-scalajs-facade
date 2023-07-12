82e2e8f61f4c96d2015def1cfecb87468b0f1e14
Filippo De Luca <me@filippodeluca.com>
2023-07-12 17:12:36 +0200

    Split in modules

---
+++ modules/core/src/main/scala/types/endpoint.scala
@@ -0,0 +1,19 @@
+package com.filippodeluca.jsfacade.awssdk.types
+
+import scalajs.js
+
+@js.native
+trait EndpointV2 extends js.Any {
+  val url: String = js.native
+
+  val headers: js.Dictionary[js.Array[String]] = js.native
+
+  /** export type EndpointObjectProperty = \| string \| boolean \| { [key:
+    * string]: EndpointObjectProperty } \| EndpointObjectProperty[];
+    *
+    * Typescript type: properties?: { authSchemes?: AuthScheme[]; } &
+    * Record<string, EndpointObjectProperty>;
+    */
+  val properties: js.UndefOr[js.Object] = js.native
+
+}
